341354a4488ba5a2137fb066d9f11d9b6423ae30
Zaid Khan <khanzaid0088@gmail.com>
2025-12-17 22:43:16 +0530

    Basic Metric Panel Created

--- interface.py
+++ interface.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import pandas as pd
 
-
 st.sidebar.title('Inventory Management Dashboard')
 option=st.sidebar.radio('Select Option',['Basic Operation','Operational Tasks'])
+
+# ------------------------------------------------- Basic Information Page -----------------------------------------
+if option=='Basic Operation':
+    st.title('Inventory and Supply Chain Management')
+    st.header('Basic Metrics')
+    
+    col1,col2,col3=st.columns(3)
+# creating KPI metric grid
+    with col1:
+        st.metric(label='Total Suppliers',value='--')
+    with col2:
+        st.metric(label='Total products',value='--')
+    with col3:
+        st.metric(label='Total Categories',value='--')
+
+# creating col 4 to 6
+    col4,col5,col6=st.columns(3)
+
+    with col4:
+        st.metric(label='Total Sale Value(Last 3 Months)',value='--')
+    with col5:
+        st.metric(label='Total Restock value(Last 3 months)',value='--')
+    with col6:
+        st.metric(label='Below Reorder and no pending Reorders',value='--')
